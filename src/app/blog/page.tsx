1fa5b6b1ea914c5dd57334bff61f0e07fb38cda8
Kevin Muhlbach <HeadbuttStudios@gmail.com>
2023-11-26 12:08:12 -0800

    add link component navigation

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: {
-    absolute: 'Blog',
+    absolute: 'Blog', // this will now ignore any suffix/prefix from the parent.
   },
 };
 
