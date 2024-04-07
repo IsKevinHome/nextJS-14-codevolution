7239fb8d5965ef5def0eadf1e7730032d662409d
Kevin M <74286858+IsKevinHome@users.noreply.github.com>
2024-04-06 21:46:45 -0700

    Delete request handlers

--- route-handlers-demo/src/app/comments/[id]/route.js
+++ route-handlers-demo/src/app/comments/[id]/route.js
@@ -25,7 +25,9 @@ export async function DELETE(request, { params }) {
     (comment) => comment.id === parseInt(params.id)
   );
 
+  const deletedComment = comments[index];
+
   comments.splice(index, 1);
 
-  return Response.json({ message: 'Comment deleted' });
+  return Response.json(deletedComment);
 }
