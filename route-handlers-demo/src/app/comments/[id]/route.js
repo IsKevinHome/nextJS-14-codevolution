c9c70854d0988df4d8df62f5243a616a65a6a967
Kevin M <74286858+IsKevinHome@users.noreply.github.com>
2024-04-06 21:43:08 -0700

    PUT request

---
+++ route-handlers-demo/src/app/comments/[id]/route.js
@@ -0,0 +1,31 @@
+import { comments } from '../data';
+
+export async function GET(request, { params }) {
+  const comment = comments.find(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  return Response.json(comment);
+}
+
+export async function PATCH(request, { params }) {
+  const body = await request.json();
+  const { text } = body;
+
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+
+  comments[index].text = text;
+
+  return Response.json(comments[index]);
+}
+
+export async function DELETE(request, { params }) {
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+
+  comments.splice(index, 1);
+
+  return Response.json({ message: 'Comment deleted' });
+}
